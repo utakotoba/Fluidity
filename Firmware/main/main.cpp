1c2293407a14b12e1ced2bdc2963593ea714e1f9
Akiyama Kohane <echo.aimless@gmail.com>
2025-09-23 13:34:33 +0800

    chore: remove wrong reference in main.cpp

--- Firmware/main/main.cpp
+++ Firmware/main/main.cpp
@@ -3,21 +3,19 @@
 #include "tcp_server.hpp"
 #include "wifi_controller.hpp"
 
-#define HTTP_RESPONSE            \
-  "HTTP/1.1 200 OK\r\n"          \
-  "Content-Type: text/plain\r\n" \
-  "Content-Length: 11\r\n"       \
-  "\r\n"                         \
-  "Hello World"
+// #define HTTP_RESPONSE            \
+//   "HTTP/1.1 200 OK\r\n"          \
+//   "Content-Type: text/plain\r\n" \
+//   "Content-Length: 11\r\n"       \
+//   "\r\n"                         \
+//   "Hello World"
 
-auto simple_handler = [](const char* data, size_t length, int client_socket) {
-  return HTTP_RESPONSE;
-};
+// auto simple_handler = [](const char* data, size_t length, int client_socket)
+// {
+//   return HTTP_RESPONSE;
+// };
 
 extern "C" void app_main() {
   controller::ensure_nvs();
   controller::wifi_connect(CONFIG_WIFI_SSID, CONFIG_WIFI_PASSWORD);
-
-  server::set_message_handler(simple_handler);
-  server::launch(8080);
 }
